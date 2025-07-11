47e4d124ac835e7feac0acbdfca7d82c3d05220b
Dhruv Sawarkar <dhruvsawarkar34@gmail.com>
2025-07-11 21:23:46 +0530

    Add files via upload

---
+++ Lab1_1.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+public class Lab1_1
+{
+    static void main(){
+        String name = "Dhruv Sawarkar";
+        String college = "WIT Solapur";
+        LocalDate date = LocalDate.now();
+        System.out.println("Name : " + name);
+        System.out.println("College Name : " + college);
+        System.out.println("Date : " + date);
+    }
+}
