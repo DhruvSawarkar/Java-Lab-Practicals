744fb70f143a26dbeb8508af9aaf3b7882f71870
Dhruv Sawarkar <dhruvsawarkar34@gmail.com>
2025-08-05 22:36:53 +0530

    Update Lab1_1.java

--- Lab 1/Lab1_1.java
+++ Lab 1/Lab1_1.java
@@ -1,1 +1,11 @@
-
+import java.utiil.LocalDate;
+publiic class Lab1_1{
+  public static void main(String[] args){
+    String name = "Dhruv Sawarkar";
+    String college = "WIT Solapur";
+    LocalDate date = LocalDate.now();
+    System.out.println("Name : " + name);
+    System.out.println("College name : " + college);
+    System.out.println("Date : " + date);
+  }
+}
