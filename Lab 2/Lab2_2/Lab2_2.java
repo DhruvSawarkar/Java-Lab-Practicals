fee0c89fdc6b026bbee97c0667c0f747edfbc947
Dhruv Sawarkar <dhruvsawarkar34@gmail.com>
2025-09-02 21:00:09 +0530

    Delete Lab 2/Lab2_2 directory

--- Lab 2/Lab2_2/Lab2_2.java
+++
@@ -1,7 +0,0 @@
-class Lab2_2{
-    public static void main(String[] args){
-        Student s = new Student("Dhruv",57,90);
-        s.print();
-        s.show();
-    }
-}
--- Lab 2/Lab2_2/Printable.java
+++
@@ -1,4 +0,0 @@
-import java.util.Scanner;
-interface Printable{
-    void print();
-}
